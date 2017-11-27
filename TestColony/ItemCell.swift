f72e62575237ef21d03f608449b96d266f92a888
Ethan <b0ss@ethans-air.newton.edu>
2017-11-27 10:56:39 -0500

    Initial Commit

---
+++ TestColony/ItemCell.swift
@@ -0,0 +1,17 @@
+//
+//  ItemCell.swift
+//  TestColony
+//
+//  Created by Ethan on 11/20/17.
+//  Copyright © 2017 TheSucc. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ItemCell: UITableViewCell {
+    @IBOutlet var nameL: UILabel!
+    @IBOutlet var serialL: UILabel!
+    @IBOutlet var valueL: UILabel!
+    
+}
