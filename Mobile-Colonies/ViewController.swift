49c88e600f13811b5a97dbc415f323664747b655
DeadMansMarch <lsp425@gmail.com>
2017-11-29 18:55:44 -0500

    Display changes.

--- Mobile-Colonies/ViewController.swift
+++ Mobile-Colonies/ViewController.swift
@@ -19,6 +19,10 @@ class ViewController: UIViewController {
     var colony_topy:Double = 0; // current colony position (top left y is 0)
     var colony_box_zoom:Double = 20; // # of boxes on horizontal axis.
     
+    var colony_bounds = (20,20);
+    var display_out = false;
+    var wrapping = false;
+    
     var cache = [Cell : UIBezierPath]();
     
     @IBOutlet var evolve_speed: UISlider!
@@ -98,41 +102,65 @@ class ViewController: UIViewController {
         );
     }
     
+    func outOfBounds(_ truex:Double,_ truey:Double)->Bool{
+        return colony_bounds.0 > 0 && colony_bounds.1 > 0 &&
+            (Int(truex) > colony_bounds.0 || Int(truey) > colony_bounds.1 || truex < 0 || truey < 0)
+    }
+    
+    func onBounds(_ truex:Double, _ truey:Double)->Bool{
+        return
+            (truex - 1 == Double(colony_bounds.0) && truey <= 1 + Double(colony_bounds.1) && truey > -1) ||
+            (truey - 1 == Double(colony_bounds.1) && truex <= Double(colony_bounds.0) && truex > -1) ||
+            (truex == -1 && truey <= Double(colony_bounds.1) + 1 && truey > -2) ||
+            (truey == -1 && truex <= Double(colony_bounds.0) + 1 && truex > -1)
+    }
+    
     func superpos(){ //This will be executed in the view's drawing methods.
         let topx = self.colony_topx;
         let topy = self.colony_topy;
         let zoom = self.colony_box_zoom;
         
         let size = ((colony_width - 20) - Double(zoom - 1)) / Double(zoom)
-        print(size)
-        for x in -2...Int(ceil(zoom))+Int(zoom / 5){
-            for y in -2...Int(ceil(zoom))+Int(zoom / 10){
+        
+        let btx = (topx - floor(topx)) * size;
+        let bty = (topy - floor(topy)) * size;
+        
+        let drawX = Int(ceil(zoom))+Int(zoom / 5);
+        let drawY = Int(ceil(zoom))+Int(zoom / 5);
+        
+        for x in -2...drawX{
+            for y in -2...drawY{
                 let truex = Double(x) + floor(topx);
                 let truey = Double(y) + floor(topy);
-                
-                let btx = (topx - floor(topx)) * size;
-                let bty = (topy - floor(topy)) * size;
-                let cell = CGRect(
-                            x:10-btx+(size*Double(x-1)),
-                            y:10-bty+(size*Double(y-1)),
-                            width: size, height:size);
-                
-                let path:UIBezierPath = UIBezierPath(rect: cell);
-                UIColor.black.setStroke();
-                if truex < 1 || truey < 1{
-                    UIColor.red.setStroke();
-                    UIColor.red.setFill()
-                    path.fill()
-                }else if current_colony.isCellAlive(X: Int(truex), Y: Int(truey)){
-                    UIColor.blue.setFill()
-                    path.fill()
+                if current_colony.isCellAlive(X: Int(truex), Y: Int(truey)) || onBounds(truex,truey){
+                    let cell = CGRect(
+                        x:10-btx+(size*Double(x-1)),
+                        y:10-bty+(size*Double(y-1)),
+                        width: size, height:size);
+                    
+                    let path:UIBezierPath = UIBezierPath(rect: cell);
+                    UIColor.white.setStroke();
+                    if !wrapping{
+                        if outOfBounds(truex,truey){
+                            UIColor.red.setStroke();
+                            UIColor.red.setFill();
+                            path.fill();
+                        }else{
+                            UIColor.blue.setFill();
+                            path.fill();
+                        }
+                    }else{
+                        
+                    }
+                    
+                    
+                    path.lineWidth = 2;
+                    path.stroke();
                 }
-                
-                path.lineWidth = 2;
-                path.stroke();
             }
-            
         }
+        
+ 
     }
     
     func redraw(){
@@ -142,7 +170,7 @@ class ViewController: UIViewController {
     @IBAction func zoom(_ sender: UIPinchGestureRecognizer) {
         print(sender);
         if let view = sender.view {
-            let newScale = colony_box_zoom * Double(sender.scale);
+            let newScale = colony_box_zoom / Double(sender.scale);
             if newScale <= 100 && newScale >= 2{
                 colony_box_zoom = newScale;
             }else if newScale > 100{
@@ -158,8 +186,8 @@ class ViewController: UIViewController {
     
     @IBAction func pan(_ sender: UIPanGestureRecognizer) {
         let translation = sender.translation(in: self.view);
-        colony_topx += Double(translation.x) / 500;
-        colony_topy += Double(translation.y) / 500;
+        colony_topx += Double(translation.x) / 100;
+        colony_topy += Double(translation.y) / 100;
         update_colony_xy(x:Int(floor(colony_topx)),y:Int(floor(colony_topy)))
         redraw();
     }
